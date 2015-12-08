6dc35e68d64ceb60094e9d6c2e4d79b820f4331f
Simon <simoncmilburn@gmail.com>
2015-12-08 11:32:05 -0500

    interactive reach

--- js/reach.js
+++ js/reach.js
@@ -1,47 +1,43 @@
-var numSegments = 14,
+var numSegments = 13,
   x = [],
   y = [],
+  x2 = [],
+  y2 = [],
+  x3 = [],
+  y3 = [],
   angle = [],
-  segLength = 22,
-  targetX, targetY,
-  ballX = 50,
-  ballY = 50,
-  ballXDirection = 1,
-  ballYDirection = -1;
+  segLength = 26,
+  targetX, targetY;
 
 for (var i = 0; i < numSegments; i++) {
   x[i] = 0;
   y[i] = 0;
+  x2[i] = 0;
+  y2[i] = 0;
+  x3[i] = 0;
+  y3[i] = 0;
   angle[i] = 0;
 }
 
 function setup() {
-  createCanvas(600, 400);
+  createCanvas(710, 400);
   strokeWeight(20);
   stroke(255, 100);
   noFill();
 
-  x[x.length-1] = width/2; // Set base x-coordinate
-  y[x.length-1] = height;  // Set base y-coordinate
+  x[x.length-1] = width/2; 
+  y[x.length-1] = height;  
+  x2[x.length-1] = width/4; 
+  y2[x.length-1] = height;
+  x3[x.length-1] = 3*width/4; 
+  y3[x.length-1] = height;
 }
 
 function draw() {
   background(0);
-
   strokeWeight(20);
-  ballX = ballX + 1.0 * ballXDirection;
-  ballY = ballY + 0.8 * ballYDirection;
-  if(ballX > width-25 || ballX < 25) {
-    ballXDirection *= -1;
-  }
-  if(ballY > height-25 || ballY < 25) {
-    ballYDirection *= -1;
-  }
-  //fill(255, 255, 0);
-  ellipse(ballX, ballY, 30, 30);
 
-  reachSegment(0, ballX, ballY);
-  //fill(69, 139, 0);
+  reachSegment(0, mouseX, mouseY);
   for(var i=1; i<numSegments; i++) {
     reachSegment(i, targetX, targetY);
   }
@@ -50,12 +46,18 @@ function draw() {
   }
   for(var k=0; k<x.length; k++) {
     segment(x[k], y[k], angle[k], (k+1)*2);
+    segment(x2[k], y2[k], angle[k], (k+1)*2);
+    segment(x3[k], y3[k], angle[k], (k+1)*2);
   }
 }
 
 function positionSegment(a, b) {
   x[b] = x[a] + cos(angle[a]) * segLength;
   y[b] = y[a] + sin(angle[a]) * segLength;
+  x2[b] = x2[a] + cos(angle[a]) * segLength;
+  y2[b] = y2[a] + sin(angle[a]) * segLength;
+  x3[b] = x3[a] + cos(angle[a]) * segLength;
+  y3[b] = y3[a] + sin(angle[a]) * segLength;
 }
 
 function reachSegment(i, xin, yin) {
